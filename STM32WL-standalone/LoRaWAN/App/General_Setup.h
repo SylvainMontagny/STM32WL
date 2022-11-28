93505009ee6ffeed41a157c8e59393a6408305a7
Sylvain Montagny <sylvain.montagny@univ-smb.fr>
2022-11-28 12:11:50 +0100

    New config_application and General_Setup

--- STM32WL-standalone/LoRaWAN/App/General_Setup.h
+++ STM32WL-standalone/LoRaWAN/App/General_Setup.h
@@ -30,15 +30,17 @@
 
 /*---------------------------------------------------------------------------------------------------------------
  ACTIVATION_MODE     |    ABP or OTAA     |    # Selection of Activation Method                                  |
- CLASS               | CLASS_A or CLASS_C |    # Class selection
+ CLASS               | CLASS_A or CLASS_C |    # Class selection												 |
+ SPREADING_FACTOR    |   Number [7;12]    |    # 7=SF7, 8=SF8, ..., 12=SF12                                      |
+ ADAPTIVE_DR         |  true or false     |    # Enable Adaptive Data Rate (if true)                             |
+ CONFIRMED           |  true or false     |    # Frame Confirmed (if true) OR Frame Unconfirmed (if false)       |
+ PORT                |   Number [0;255]   |    # Application Port number                                         |
+                       	   	   	   	   	   	   	   	   	   	   	   	   	   	   	   	   	   	   	   	   	   	   	 |
  SEND_BY_PUSH_BUTTON |   true or false    |    # Sending method (Time or Push Button)                            |
  FRAME_DELAY         |    Time in ms      |    # Time between 2 frames (Minimum 7000)                            |
- SPREADING_FACTOR    |   Number [7;12]    |    # 7=SF7, 8=SF8, ..., 12=SF12                                      |
- ADAPTIVE_DR         |  true or false     |    # Enable Adaptive Data Rate (if true)                              |
- CONFIRMED           |  true or false     |    # Frame Confirmed (if true) OR Frame Unconfirmed (if false)       |
- PORT                |   Number [0;199]   |    # Application Port number                                         |
- ENABLE_HUMIDITY     |  true or false     |    # Enable Humidity Sensor (if true)                                |
- CAYENNE_LPP         |  true or false     |    # Enable all sensor and use the CAYENNE LPP format (if true)      |
+ PAYLOAD_HELLO       |  true or false     |    # Sends the string HELLO : 48 45 4C 4C 4F (hex)                   |
+ PAYLOAD_TEMPERATURE |  true or false     |    # Sends a simulated temperature between 20 and 25 degrees         |
+ CAYENNE_LPP_        |  true or false     |    # Enable all sensors and use the CAYENNE LPP format (if true)     |
  LOW_POWER           |  true or false     |    # Enable Low Power mode between two frames (if true)              |
 ---------------------------------------------------------------------------------------------------------------*/
 
@@ -147,7 +149,7 @@
 /* LoRaWAN Application --------------------------------------------------------*/
 
 // Uplink Mode
-#define ADMIN_TX_TYPE							SEND_BY_PUSH_BUTTON				// possible values: 0 (by timer, every TxDutyCycleTime) / 1 (by button) - lora_app.c
+#define ADMIN_TX_TYPE							SEND_BY_PUSH_BUTTON				// possible values: 0 (by timer, every TxDutyCycleTime) / 1 (by button B3) - lora_app.c
 #define ADMIN_TxDutyCycleTime					FRAME_DELAY						// in ms - lora_app.h
 #define ADMIN_TX_CONFIRMED_TYPE					CONFIRMED						// possible values: LORAMAC_HANDLER_UNCONFIRMED_MSG / LORAMAC_HANDLER_CONFIRMED_MSG - lora_app.h
 
@@ -180,7 +182,7 @@
 #define GREEN				"\033[32m"			// Green
 #define YELLOW  			"\033[33m"      	/* Yellow */
 #define BLUE    			"\033[34m"      	/* Blue */
-#define MAGENTA 			"\033[35m"      	/* Magenta */
+#define MAGENTA 			"\033[35m"      	/* Magenta */○
 #define CYAN    			"\033[36m"      	/* Cyan */
 #define WHITE   			"\033[37m"      	/* White */
 #define BOLDBLACK   		"\033[1m\033[30m"      /* Bold Black */
--- STM32WL-standalone/LoRaWAN/config_application.h
+++ STM32WL-standalone/LoRaWAN/config_application.h
@@ -1,33 +1,17 @@
 
-#define ACTIVATION_MODE     		OTAA
+#define ACTIVATION_MODE     		ABP
 #define CLASS						CLASS_A
-#define SEND_BY_PUSH_BUTTON 		false
-#define FRAME_DELAY         		8000
 #define SPREADING_FACTOR    		7
 #define ADAPTIVE_DR         		false
 #define CONFIRMED           		false
 #define PORT                		15
+
+#define SEND_BY_PUSH_BUTTON 		false
+#define FRAME_DELAY         		10000
 #define PAYLOAD_HELLO				true
 #define PAYLOAD_TEMPERATURE    		false
 #define CAYENNE_LPP_         		false
 #define LOW_POWER           		false
-
-
-
-#define devEUI_						{ 0xXX, 0xXX, 0xXX, 0xXX, 0xXX, 0xXX, 0xXX, 0xXX }
-
-// Configuration for ABP Activation Mode
-#define devAddr_ 					( uint32_t )0xXXXXXXXX
-#define nwkSKey_ 					XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX
-#define appSKey_ 					XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX
-
-
-// Configuration for OTAA Activation Mode
-#define appKey_						XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX
-#define appEUI_						{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
-
-
-
 
 
 
