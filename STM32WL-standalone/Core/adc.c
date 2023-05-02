ea9f56ced3634cb5415b58dd9856e2c3c73c8b17
Sylvain Montagny <sylvain.montagny@univ-smb.fr>
2023-05-02 17:43:02 +0200

    Clean code

--- STM32WL-standalone/Core/adc.c
+++ STM32WL-standalone/Core/adc.c
@@ -1,44 +1,11 @@
-/**
-  ******************************************************************************
-  * @file    adc.c
-  * @brief   This file provides code for the configuration
-  *          of the ADC instances.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
-  *
-  ******************************************************************************
-  */
 
-/* Includes ------------------------------------------------------------------*/
 #include "adc.h"
-
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
 
 ADC_HandleTypeDef hadc;
 
-/* ADC init function */
 void MX_ADC_Init(void)
 {
 
-  /* USER CODE BEGIN ADC_Init 0 */
-
-  /* USER CODE END ADC_Init 0 */
-
-  /* USER CODE BEGIN ADC_Init 1 */
-
-  /* USER CODE END ADC_Init 1 */
-  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-  */
   hadc.Instance = ADC;
   hadc.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
   hadc.Init.Resolution = ADC_RESOLUTION_12B;
@@ -62,9 +29,6 @@ void MX_ADC_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN ADC_Init 2 */
-
-  /* USER CODE END ADC_Init 2 */
 
 }
 
@@ -73,14 +37,8 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
 
   if(adcHandle->Instance==ADC)
   {
-  /* USER CODE BEGIN ADC_MspInit 0 */
-
-  /* USER CODE END ADC_MspInit 0 */
-    /* ADC clock enable */
     __HAL_RCC_ADC_CLK_ENABLE();
-  /* USER CODE BEGIN ADC_MspInit 1 */
 
-  /* USER CODE END ADC_MspInit 1 */
   }
 }
 
@@ -89,19 +47,8 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle)
 
   if(adcHandle->Instance==ADC)
   {
-  /* USER CODE BEGIN ADC_MspDeInit 0 */
-
-  /* USER CODE END ADC_MspDeInit 0 */
-    /* Peripheral clock disable */
     __HAL_RCC_ADC_CLK_DISABLE();
-  /* USER CODE BEGIN ADC_MspDeInit 1 */
 
-  /* USER CODE END ADC_MspDeInit 1 */
   }
 }
 
-/* USER CODE BEGIN 1 */
-
-/* USER CODE END 1 */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- STM32WL-standalone/Core/rtc.c
+++ STM32WL-standalone/Core/rtc.c
@@ -1,46 +1,16 @@
-/**
-  ******************************************************************************
-  * @file    rtc.c
-  * @brief   This file provides code for the configuration
-  *          of the RTC instances.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
-  *
-  ******************************************************************************
-  */
 
-/* Includes ------------------------------------------------------------------*/
 #include "rtc.h"
 
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
 
 RTC_HandleTypeDef hrtc;
 
-/* RTC init function */
+
 void MX_RTC_Init(void)
 {
 
-  /* USER CODE BEGIN RTC_Init 0 */
-
-  /* USER CODE END RTC_Init 0 */
 
   RTC_AlarmTypeDef sAlarm = {0};
 
-  /* USER CODE BEGIN RTC_Init 1 */
-
-  /* USER CODE END RTC_Init 1 */
-  /** Initialize RTC Only
-  */
   hrtc.Instance = RTC;
   hrtc.Init.AsynchPrediv = RTC_PREDIV_A;
   hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
@@ -54,12 +24,7 @@ void MX_RTC_Init(void)
     Error_Handler();
   }
 
-  /* USER CODE BEGIN Check_RTC_BKUP */
 
-  /* USER CODE END Check_RTC_BKUP */
-
-  /** Initialize RTC and set the Time and Date
-  */
   if (HAL_RTCEx_SetSSRU_IT(&hrtc) != HAL_OK)
   {
     Error_Handler();
@@ -75,9 +40,6 @@ void MX_RTC_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN RTC_Init 2 */
-
-  /* USER CODE END RTC_Init 2 */
 
 }
 
@@ -87,11 +49,7 @@ void HAL_RTC_MspInit(RTC_HandleTypeDef* rtcHandle)
   RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
   if(rtcHandle->Instance==RTC)
   {
-  /* USER CODE BEGIN RTC_MspInit 0 */
 
-  /* USER CODE END RTC_MspInit 0 */
-  /** Initializes the peripherals clocks
-  */
     PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_RTC;
     PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
 
@@ -109,9 +67,6 @@ void HAL_RTC_MspInit(RTC_HandleTypeDef* rtcHandle)
     HAL_NVIC_EnableIRQ(TAMP_STAMP_LSECSS_SSRU_IRQn);
     HAL_NVIC_SetPriority(RTC_Alarm_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(RTC_Alarm_IRQn);
-  /* USER CODE BEGIN RTC_MspInit 1 */
-
-  /* USER CODE END RTC_MspInit 1 */
   }
 }
 
@@ -120,24 +75,15 @@ void HAL_RTC_MspDeInit(RTC_HandleTypeDef* rtcHandle)
 
   if(rtcHandle->Instance==RTC)
   {
-  /* USER CODE BEGIN RTC_MspDeInit 0 */
 
-  /* USER CODE END RTC_MspDeInit 0 */
-    /* Peripheral clock disable */
     __HAL_RCC_RTC_DISABLE();
     __HAL_RCC_RTCAPB_CLK_DISABLE();
 
     /* RTC interrupt Deinit */
     HAL_NVIC_DisableIRQ(TAMP_STAMP_LSECSS_SSRU_IRQn);
     HAL_NVIC_DisableIRQ(RTC_Alarm_IRQn);
-  /* USER CODE BEGIN RTC_MspDeInit 1 */
 
-  /* USER CODE END RTC_MspDeInit 1 */
   }
 }
 
-/* USER CODE BEGIN 1 */
 
-/* USER CODE END 1 */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
