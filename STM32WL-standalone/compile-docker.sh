4e0ffdf88da2fd7adb29b938cb8cb7499b593ff6
Sylvain Montagny <sylvain.montagny@univ-smb.fr>
2024-06-08 07:55:04 +0200

    Fix make clean

--- STM32WL-standalone/compile-docker.sh
+++ STM32WL-standalone/compile-docker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # This container needs to be run from the STM32WL/STM32WL-standalone folder
-sudo make clean
-sudo docker run --rm -v "$PWD/":/workspace montagny/arm-compiler 
+sudo docker run --rm -v "$PWD/":/workspace montagny/arm-compiler:1.0  /bin/bash -c "make clean && make"
 
